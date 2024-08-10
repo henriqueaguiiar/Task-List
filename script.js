71405dd36105858bdf93f72edd9b1a4d2d530086
Henrique Aguiar <henriqueaguiarpacheco09@gmail.com>
2024-08-10 01:10:28 -0300

    Segundo commit estilizando botão e adicionando funcionalidade

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const inputElement = document.querySelector(".new-task-input");
 const addTaskButton = document.querySelector(".new-task-button");
 
+const tasksContainer = document.querySelector(".tasks-container");
+
 const validateInput = () => inputElement.value.trim().length > 0;
 
 const handleAddTask = () => {
@@ -9,6 +11,37 @@ const handleAddTask = () => {
   if (!inputIsValid) {
     return inputElement.classList.add("error");
   }
+
+  const taskItemContainer = document.createElement("div");
+  taskItemContainer.classList.add("task-item");
+
+  const taskContent = document.createElement("p");
+  taskContent.innerText = inputElement.value;
+
+  taskContent.addEventListener("click", () => handleClick());
+
+  const deleteItem = document.createElement("i");
+  deleteItem.classList.add("far");
+  deleteItem.classList.add("fa-trash-alt");
+
+  deleteItem.addEventeListener("click", () => handleDeleteClicl());
+
+  taskItemContainer.appendChild(taskContent);
+  taskItemContainer.appendChild(deleteItem);
+
+  tasksContainer.appendChild(taskItemContainer);
+
+  inputElement.value = "";
+};
+
+const handleInputChange = () => {
+  const inputIsValid = validateInput();
+
+  if (inputIsValid) {
+    return inputElement.classList.remove("error");
+  }
 };
 
 addTaskButton.addEventListener("click", () => handleAddTask());
+
+inputElement.addEventListener("change", () => handleInputChange());
